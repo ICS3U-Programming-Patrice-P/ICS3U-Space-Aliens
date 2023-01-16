4244d48fd257cf1a29915df8bbded9da39728d0a
Patrice Pat-Odita <patrice.pat-odita@stu.ocsb.ca>
2023-01-16 19:31:45 +0000

    Added code.py2

--- code.py
+++ code.py
@@ -14,26 +14,34 @@ def game_scene():
     # this is the main scene for space alien
     #image bank for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
-
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
     #set the background to image 0 in the image bank
     #   and the size
     background = stage.Grid(image_bank_background, 10, 8)
+    # updates the sprite in every frame
+    ship = stage.Sprite(image_bank_sprites, 4, 75, 66)
 
-    #create a stage for the background to show up on
-    #   and set the frame rate to 60 fps
+    # creates a stage background and sets the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
 
-    #set the layers of all the sprites, items show up in order
-    game.layers = [background]
+    #sets the layers of all the sprites and items to show up in order
+    game.layers = [ship] + [background]
 
-    #render all sprites
+    #renders all sprites
     game.render_block()
 
     #repeat forever game loop
 
     while True:
-        pass
+        # gets user input
+        # updates the game logic
+        
+        # redraw sprite
+        game.render_sprites([ship])
+        game.tick()
+
 
 
 if __name__ == "__main__":
     game_scene()
+
